f41db101e4d3fa34dd883e35d0eadf7d9cc96977
exectionik <jakkappro@gmail.com>
2022-03-23 09:17:22 +0100

    Add project files.

---
+++ Qken2/Qken2/DudoClicker.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Qken2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DudoClicker : ContentPage
+    {
+        public DudoClicker()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ Qken2/Qken2/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Qken2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new DudoClicker());
+        }
+    }
+}
