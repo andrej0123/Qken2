59863ed1c4621c7850926182a6ff0464c6c1c8f8
Andrej Svrček <nepoviem251@gmail.com>
2022-03-23 20:30:16 +0100

    Dudo Clicker

--- Qken2/Qken2/DudoClicker.xaml.cs
+++ Qken2/Qken2/DudoClicker.xaml.cs
@@ -12,6 +12,12 @@ namespace Qken2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DudoClicker : ContentPage
     {
+
+        int Dudcoin = 0;
+        int x = 1;
+        int i = 0;
+        int j = 0;
+
         public DudoClicker()
         {
             InitializeComponent();
@@ -20,6 +26,38 @@ namespace Qken2
         private void Button_Clicked(object sender, EventArgs e)
         {
             Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void Button_Clicked_0(object sender, EventArgs e)
+        {
+            Dudcoin += x;
+            Dudlabel.Text = Dudcoin.ToString();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if (Dudcoin >= 100)
+            {
+                i += 1;
+                if (i <= 1)
+                {
+                    Dudcoin -= 100;
+                    x = 2;
+                }
+            }
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            if (Dudcoin >= 500)
+            {
+                j += 1;
+                if (j <= 1)
+                {
+                    Dudcoin -= 500;
+                    x = 3;
+                }
+            }
         }
     }
 }
